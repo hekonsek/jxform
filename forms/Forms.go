f9682b384f3751bfd42307b6817d5a396eac3816
Henryk Konsek <hekonsek@gmail.com>
2018-12-17 15:52:52 +0100

    Added jx installation.

--- forms/Forms.go
+++ forms/Forms.go
@@ -7,12 +7,45 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 )
 
 type FormDefinition struct {
 	Name         string        `yaml:"name"`
+	Domain       string        `yaml:"domain"`
+	Git          Git           `yaml:"git"`
 	Environments []Environment `yaml:"environments"`
+}
+
+func (def FormDefinition) ResolveName() string {
+	name, found := os.LookupEnv("NAME")
+	if found {
+		return name
+	}
+	return def.Name
+}
+
+type Git struct {
+	Server   string `yaml:"server"`
+	Username string `yaml:"username"`
+	Token    string `yaml:"token"`
+}
+
+func (git Git) ResolveUsername() string {
+	username, found := os.LookupEnv("GIT_USERNAME")
+	if found {
+		return username
+	}
+	return git.Username
+}
+
+func (git Git) ResolveToken() string {
+	token, found := os.LookupEnv("GIT_TOKEN")
+	if found {
+		return token
+	}
+	return git.Token
 }
 
 type Environment struct {
@@ -30,6 +63,7 @@ func Provision(verbose bool) error {
 	if err != nil {
 		return err
 	}
+	definitionName := definition.ResolveName()
 
 	eksGetCommand := []string{"get", "eks"}
 	if verbose {
@@ -41,13 +75,13 @@ func Provision(verbose bool) error {
 	}
 	hasCluster := false
 	for _, line := range out {
-		if strings.HasPrefix(line, definition.Name+"\t") {
+		if strings.HasPrefix(line, definitionName+"\t") {
 			hasCluster = true
 			break
 		}
 	}
 	if !hasCluster {
-		eksCreateCommand := []string{"create", "cluster", "eks", "--cluster-name=" + definition.Name, "--skip-installation=true"}
+		eksCreateCommand := []string{"create", "cluster", "eks", "--cluster-name=" + definitionName, "--skip-installation=true"}
 		eksCreateCommand = append(eksCreateCommand, fmt.Sprintf("--verbose=%t", verbose))
 		if verbose {
 			fmt.Printf("About to execute command: %s\n", append([]string{"jx"}, eksCreateCommand...))
@@ -57,7 +91,37 @@ func Provision(verbose bool) error {
 			log.Fatal(err)
 		}
 	} else {
-		fmt.Printf("Cluster %s already exists.\n", color.GreenString(definition.Name))
+		fmt.Printf("EKS cluster %s already exists.\n", color.GreenString(definitionName))
+	}
+
+	helmList, err := util.NewExecs().Run("helm", "list")
+	if err != nil {
+		log.Fatal(err)
+	}
+	isJxInstalled := false
+	for i, line := range helmList {
+		if i == 0 {
+			continue
+		}
+		if strings.HasPrefix(line, "jenkins-x\t") {
+			isJxInstalled = true
+			break
+		}
+	}
+	if !isJxInstalled {
+		installCommand := []string{"install", "--provider=eks", "-b",
+			"--git-api-token=" + definition.Git.ResolveToken(), "--git-provider-url=" + definition.Git.Server, "--git-private=true", "--git-username=" + definition.Git.ResolveUsername(),
+			"--default-environment-prefix=" + definitionName, "--no-default-environments=true", "--domain=" + definition.Domain}
+		installCommand = append(installCommand, fmt.Sprintf("--verbose=%t", verbose))
+		if verbose {
+			fmt.Printf("About to execute command: %s\n", append([]string{"jx"}, installCommand...))
+		}
+		err = util.NewExecs().Sout("jx", installCommand...)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Printf("Jenkins X already installed.\n")
 	}
 
 	return nil
